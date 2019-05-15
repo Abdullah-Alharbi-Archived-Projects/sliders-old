fe5dcf4c6f709d847dd383275fd7964e9d473f35
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-16 02:42:34 +0300

    inital steps

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// this will select section#slider
+const slider = document.getElementById('slider');
+// this will select ul.slide-track
+const slideTrack = document.querySelector('ul.slide-track');
+
+// this will get this attribute 'data-seconds' from section#slider
+// if not exists then set this to 15 seconds
+const seconds = slider.hasAttribute('data-seconds') ? slider.getAttribute('data-seconds') : 15;
+
+// this will get all the sliders(li.slide) from ul.slide-track
+const slides = Array.from(slideTrack.children);
