303328bc0c83a45f6f27c2f9fcb98e0f7f653064
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-17 04:41:43 +0300

    update weather slide

--- js/slider.js
+++ js/slider.js
@@ -10,6 +10,9 @@ const seconds = slider.hasAttribute('data-seconds') ? slider.getAttribute('data-
 // this will get all the sliders(li.slide) from ul.slide-track
 const slides = Array.from(slideTrack.children);
 
+// detect weather slide
+const weatherSlide = slideTrack.querySelector('.weather');
+
 // get the slide width
 const { width } = slides[0].getBoundingClientRect();
 
@@ -27,15 +30,37 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
 
 let firstSlide = null;
 let lastSlide = null; 
+
 // this will fire after x seconds
 setInterval(() => {
   const currentSlide = slideTrack.querySelector('.current-slide');
+
   if (!firstSlide) {
     currentSlide.id = 'first';
     firstSlide = currentSlide.id;
   }
 
   const nextSlide = currentSlide.nextElementSibling;
+  if (nextSlide === weatherSlide && weatherSlide.hasAttribute('data-weather') && !lastSlide) {
+    // next slide is the weather slide
+    if (weatherSlide.getAttribute('data-weather') === 'true') {
+      if (navigator.onLine) {
+        // there's connection!
+        // update weather stats after x seconds
+        setTimeout(function () {
+          weather(weatherSlide.getAttribute('data-city'), weatherSlide.getAttribute('data-temperature-type'), weatherSlide.getAttribute('api-key'));
+        }, 1000 * seconds);
+      } else {
+        // back previous
+        currentSlide.id = 'last';
+        lastSlide = currentSlide.id;
+      }
+    } else {
+      // back previous
+      currentSlide.id = 'last';
+      lastSlide = currentSlide.id;
+    }
+  }
   if (nextSlide && !lastSlide) {
     // move to the next slide
     moveToSlide(slideTrack, currentSlide, nextSlide);
@@ -54,3 +79,12 @@ setInterval(() => {
     }
   }
 }, 1000 * seconds);
+
+
+
+// initialize weather slide if the clien can access internet
+if (navigator.onLine) {
+  window.onload = function () {
+    weather(weatherSlide.getAttribute('data-city'), weatherSlide.getAttribute('data-temperature-type'), weatherSlide.getAttribute('api-key'));
+  }
+}
