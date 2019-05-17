28df3e1370ef3c337e27fa72b916e6805b52116b
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-18 00:03:22 +0300

    another fix

--- js/currentPray.js
+++ js/currentPray.js
@@ -1,70 +1,116 @@
 function convertTimeToUnix(time) {
   var now = new Date();
-  var h = time.split(':')[0];
-  var min = time.replace(/[a-z.ص.م]/gi, '').split(':')[1];
+  var h = time.split(":")[0];
+  var min = time.replace(/[a-z.ص.م]/gi, "").split(":")[1];
   var md = getModifier(time);
-  var splittedNowStr = now.toDateString().split(' ');
+  var splittedNowStr = now.toDateString().split(" ");
 
   // convert str time to date object
-  var ending = new Date(splittedNowStr[1] + ' ' + splittedNowStr[2] + ', ' + splittedNowStr[3] + ' ' + h + ':' + min.replace(' ', '') + ':00 ' + md);
+  var ending = new Date(
+    splittedNowStr[1] +
+      " " +
+      splittedNowStr[2] +
+      ", " +
+      splittedNowStr[3] +
+      " " +
+      h +
+      ":" +
+      min.replace(" ", "") +
+      ":00 " +
+      md
+  );
   var unixTimeEnding = ending.getTime() / 1000;
   return unixTimeEnding;
 }
 
-function getModifier(time, locale = 'en') {
-  var splittedTime = time.split(' ');
+function getModifier(time, locale = "en") {
+  var splittedTime = time.split(" ");
   var modifier = splittedTime[1];
 
-  if (locale === 'en') {
-    return modifier === 'ص' ? modifier.replace('ص', 'am') : modifier.replace('م', 'pm');
+  if (locale === "en") {
+    return modifier === "ص"
+      ? modifier.replace("ص", "am")
+      : modifier.replace("م", "pm");
   } else {
-    return modifier === 'am' ? modifier.replace('am', 'ص') : modifier.replace('pm', 'ص');
+    return modifier === "am"
+      ? modifier.replace("am", "ص")
+      : modifier.replace("pm", "ص");
   }
 }
 
-
 function countDown(time, element) {
-  var x = setInterval(function () {
+  var x = setInterval(function() {
     var now = new Date();
-    var h = time.split(':')[0];
-    var min = time.replace(/[a-z.ص.م]/gi, '').split(':')[1];
+    var h = time.split(":")[0];
+    var min = time.replace(/[a-z.ص.م]/gi, "").split(":")[1];
     var md = getModifier(time);
-    var splittedNowStr = now.toDateString().split(' ');
+    var splittedNowStr = now.toDateString().split(" ");
     // convert str time to date object
-    var ending = new Date(splittedNowStr[1] + ' ' + splittedNowStr[2] + ', ' + splittedNowStr[3] + ' ' + h + ':' + min.replace(' ', '') + ':00 ' + md);
+    var ending = new Date(
+      splittedNowStr[1] +
+        " " +
+        splittedNowStr[2] +
+        ", " +
+        splittedNowStr[3] +
+        " " +
+        h +
+        ":" +
+        min.replace(" ", "") +
+        ":00 " +
+        md
+    );
 
     var distance = ending.getTime() - now.getTime();
 
     // Time calculations for days, hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    element.innerHTML = hours + ' : ' + minutes + ' : ' + (seconds + 2) + '<br>'
-      + 'س : ' + 'د : ' + 'ث';
+    element.innerHTML =
+      hours +
+      " : " +
+      minutes +
+      " : " +
+      (seconds + 2) +
+      "<br>" +
+      "س : " +
+      "د : " +
+      "ث";
 
     if (distance < 0) {
       clearInterval(x);
-      element.innerHTML = 'حان الان وقت الصلاة';
+      element.innerHTML =
+        hours +
+        " : " +
+        minutes +
+        " : " +
+        (seconds + 2) +
+        "<br>" +
+        "س : " +
+        "د : " +
+        "ث";
       setTimeout(function() {
         run();
-      }, 1000 * 2)
+      }, 1000 * 2);
     }
   }, 1000);
 }
 
 var currentPrayObject = {
-  name: '',
-  time: ''
+  name: "",
+  time: ""
 };
 
 function run() {
+  var now = new Date();
+  var nowUnix = now.getTime() / 1000;
   for (i in list) {
     var prayUnix = convertTimeToUnix(times[list[i].toLowerCase()]);
-    var now = new Date();
-    var nowUnix = now.getTime() / 1000;
     var diff = prayUnix - nowUnix;
-  
+
     if (diff <= 0) {
       continue;
     } else {
@@ -76,8 +122,11 @@ function run() {
 }
 
 run();
-var currentPrayCountDownElement = document.getElementById('current-pray-countdown');
+console.log(currentPrayObject);
+var currentPrayCountDownElement = document.getElementById(
+  "current-pray-countdown"
+);
 countDown(currentPrayObject.time, currentPrayCountDownElement);
 
-var currentPrayElement = document.getElementById('current-pray');
+var currentPrayElement = document.getElementById("current-pray");
 currentPrayElement.innerHTML = currentPrayObject.name;
